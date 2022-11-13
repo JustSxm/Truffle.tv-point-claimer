ce21220e5dc15119c2b2b8da6bf4cd4e1bcc9047
Sam <42727829+JustSxm@users.noreply.github.com>
2022-11-12 23:42:09 -0500

    Create truffle-tv-channel-points-claimer.user.js

---
+++ truffle-tv-channel-points-claimer.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         Truffle TV Channel Points Claimer
+// @namespace    https://greasyfork.org/users/649
+// @version      1.0
+// @description  Auto claim point on Truffle TV enabled streams.
+// @author       JustSxm, Adrien Pyke (Original Author)
+// @match        *://new.ludwig.social/channel-points
+// @match        *://*.spore.build/component-instance/*
+// @grant        none
+// ==/UserScript==
+
+(() => {
+  'use strict';
+
+  setInterval(() => {
+    const root = document.getElementById('root');
+    const shadowRoot = root && root.firstChild.shadowRoot;
+    const node = shadowRoot
+      ? shadowRoot.querySelector('.claim')
+      : document.querySelector('.claim.is-visible');
+    if (node) node.click();
+  }, 1000);
+})();
